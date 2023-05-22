cc6a36396b48d3d86047c2ff3b1ca5dcca09924e
Sunil H N <sunilh.n27@gmail.com>
2023-05-23 00:38:57 +0530

    removed Auth and changed font

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,12 +3,10 @@ import React from "react";
 import Image from "next/image";
 import logo from "../image/logo-1.png";
 import { useState } from "react";
-import { useSession, signIn, signOut } from "next-auth/react";
 
 function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  const { data: session } = useSession();
 
   return (
     <div className="text-white bg-slate-700">
@@ -17,36 +15,10 @@ function NavBar() {
           <Image className="h-16 w-16" src={logo} alt="img" />
         </Link>
         <div className="hidden md:flex space-x-5 items-center  font-bold">
-          <Link href={"/"} className="hover:text-orange-400">
-            Home
-          </Link>
-          <Link href={"/experience"} className="hover:text-orange-400">
-            Experience
-          </Link>
-          <Link href={"/contactme"} className="hover:text-orange-400">
-            Contact Me
-          </Link>
-          <Link href={"/projects"} className="hover:text-orange-400">
-            Projects
-          </Link>
-          {session ? (
-            <>
-              <p className="hover:text-orange-400 pl-4">{session.user.name}</p>
-              <button
-                onClick={signOut}
-                className="bg-orange-400  px-2 py-1 font-bold rounded-lg"
-              >
-                Sign Out
-              </button>
-            </>
-          ) : (
-            <button
-              onClick={signIn}
-              className="bg-orange-400  px-2 py-1 font-bold rounded-lg"
-            >
-              Sign In
-            </button>
-          )}
+          <Link href={"/"} className="hover:text-orange-400">Home</Link>
+          <Link href={"/experience"} className="hover:text-orange-400">  Experience</Link>
+          <Link href={"/contactme"} className="hover:text-orange-400">Contact Me</Link>
+          <Link href={"/projects"} className="hover:text-orange-400">Projects</Link>
           <i className=" text-white fa-solid fa-bars text-2xl"></i>
         </div>
         <div className="md:hidden">
@@ -64,48 +36,10 @@ function NavBar() {
           </button>
           <div className={`md:hidden ${mobileMenuOpen ? "block" : "hidden"}`}>
             <div className={`md:hidden ${mobileMenuOpen ? "block" : "hidden"}`}>
-              <Link href={"/"} className="block py-2 hover:text-orange-400">
-                {" "}
-                Home
-              </Link>
-              <Link
-                href={"/experience"}
-                className="block py-2 hover:text-orange-400"
-              >
-                Experience
-              </Link>
-              <Link
-                href={"/contactme"}
-                className="block py-2 hover:text-orange-400"
-              >
-                Contact Me
-              </Link>
-              <Link
-                href={"/projects"}
-                className="block py-2 hover:text-orange-400"
-              >
-                Projects
-              </Link>
-              {session ? (
-                <>
-                  <p className="block py-2 pl-4 hover:text-orange-400">
-                    {session.user.name}
-                  </p>
-                  <button
-                    onClick={signOut}
-                    className="block bg-orange-400 px-2 py-1 font-bold rounded-lg"
-                  >
-                    Sign Out
-                  </button>
-                </>
-              ) : (
-                <button
-                  onClick={signIn}
-                  className="block bg-orange-400 px-2 py-1 font-bold rounded-lg"
-                >
-                  Sign In
-                </button>
-              )}
+              <Link href={"/"} className="block py-2 hover:text-orange-400">Home</Link>
+              <Link href={"/experience"} className="block py-2 hover:text-orange-400">Experience</Link>
+              <Link href={"/contactme"} className="block py-2 hover:text-orange-400">Contact Me</Link>
+              <Link href={"/projects"} className="block py-2 hover:text-orange-400">Projects</Link>
             </div>
           </div>
         </div>
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,19 +1,17 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 
-
 export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        }),
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-          })
-        // ...add more providers here
-    ],
-}
-export default NextAuth(authOptions)
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+};
+export default NextAuth(authOptions);
