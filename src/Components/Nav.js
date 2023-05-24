5de23421eee07e7d8d13f7814021dc0b6a6bc435
Sunil H N <sunilh.n27@gmail.com>
2023-05-25 00:55:02 +0530

    changes in NavBar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 function NavBar() {
   const [navbar, setNavbar] = useState(false);
+
   return (
     <div>
       <nav className="w-full bg-black fixed top-0 left-0 right-0 z-10">
@@ -37,7 +38,7 @@ function NavBar() {
           </div>
           <div>
             <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${ navbar ? 'p-12 md:p-0 block' : 'hidden'}`}
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'p-12 md:p-0 block' : 'hidden'}`}
             >
               <ul className="h-screen md:h-auto items-center justify-center md:flex ">
                 <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
